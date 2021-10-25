5bb20ec233943eef53daaf14bd7b52dfa53c41ac
Success Aje <successaje7@gmail.com>
2021-10-25 13:02:14 +0100

    creating a new template for my registration model

--- plum_root/quotes/urls.py
+++ plum_root/quotes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from . import views
-from .views import QuoteList
+from .views import QuoteList, QuoteView
 
 urlpatterns = [
     path('', views.quote_req, name = 'quote-request'),
-    path('show', QuoteList.as_view(), name = 'show-quotes'),
+    path('show/<int:pk>', QuoteView.as_view(), name='quote-detail'),
+    path('/show', QuoteList.as_view(), name = 'show-quotes'),
 ]
--- plum_root/quotes/views.py
+++ plum_root/quotes/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Quote
 from .forms import QuoteForm
 from pages.models import Page
+
+class QuoteView(DetailView):
+    model = Quote
+    context_object_name = 'quote'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuoteView, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
 
 class QuoteList(ListView):
     model = Quote
