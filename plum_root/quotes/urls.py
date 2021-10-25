1df254b43254f3b3445e2cb678db92cb23c72a9d
Success Aje <successaje7@gmail.com>
2021-10-25 15:42:12 +0100

    There were errors , so had to correct the errorss. so far the website is working perfectly

--- plum_root/quotes/urls.py
+++ plum_root/quotes/urls.py
@@ -6,5 +6,5 @@ from .views import QuoteList, QuoteView
 urlpatterns = [
     path('', views.quote_req, name = 'quote-request'),
     path('show/<int:pk>', QuoteView.as_view(), name='quote-detail'),
-    path('/show', QuoteList.as_view(), name = 'show-quotes'),
+    path('show', QuoteList.as_view(), name = 'show-quotes'),
 ]
